ad969d61d8b12fb30bc280b63c6930ced2c8dd71
Сергей Олефир < >
2016-05-24 15:17:48 +0300

    COnsole application

--- Johnson/Program.cs
+++ Johnson/Program.cs
@@ -8,55 +8,36 @@ namespace Johnson
     {
         static void Main(string[] args)
         {
-            //var graph = new Graph<int>();
-            //graph.Add(1, new Dictionary<int, int>()
-            //{
-            //    { 2, 10 },
-            //    { 3, 18 },
-            //    { 4, 8 }
-            //});
+            string enteredValue = string.Empty;
 
-            //graph.Add(2, new Dictionary<int, int>()
-            //{
-            //    { 1, 10 },
-            //    { 3, 16 },
-            //    { 4, 9 },
-            //    { 5, 21 }
-            //});
+            int n = 0;
+            int ves = 0;
 
-            //graph.Add(3, new Dictionary<int, int>()
-            //{
-            //    { 2, 16 },
-            //    { 6, 15 }
-            //});
-
-            //graph.Add(4, new Dictionary<int, int>()
-            //{
-            //    { 1, 7 },
-            //    { 2, 9 },
-            //    { 6, 12 }
-            //});
-
-            //graph.Add(5, new Dictionary<int, int>()
-            //{
-            //    { 6, 23 }
-            //});
-
-            //graph.Add(6, new Dictionary<int, int>()
-            //{
-            //    { 3, 15 },
-            //    { 5, 23 }
-            //});
-
-            int?[,] graph = new int?[,]
+            Console.Write("Enter count of vertexes -> ");
+            try
             {
-                { null, 10, 18, 8, null, null },
-                { 10, null, 16, 9, 21, null },
-                { null, 16, null, null, null, -15 },
-                { 7, 9, null, null, null, 12 },
-                { null, null, null, null, null, 23 },
-                { null, null, 15, null, 23, null }
-            };
+                n = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Entered value has incorrect format");
+                Console.WriteLine("Program will be terminated");
+            }
+
+            Console.WriteLine("Enter all edges one-by-one");
+            Console.WriteLine("Enter any symbol if edge don't exist");
+
+            int?[,] graph = new int?[n, n];
+
+            for(var i = 0; i < n; i++)
+            {
+                for (var j = 0; j < n; j++)
+                {
+                    Console.Write($"Enter ves between ({i},{j})-> ");
+                    enteredValue = Console.ReadLine();
+                    graph[i, j] = int.TryParse(enteredValue, out ves) ? ves : (int?)null;
+                }
+            }
 
             var dictGraph = new Graph<int>();
 
